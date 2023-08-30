33483764ad4d82e1aa3000dc8588af6c24b10bbb
Your Name <jymohcooler@gmail.com>
2023-08-30 13:45:05 +0300

    complete project

---
+++ lib/migrations/versions/4f7006ff742c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4f7006ff742c
+Revises: 
+Create Date: 2023-08-30 12:47:02.554825
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4f7006ff742c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
